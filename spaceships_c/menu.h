007b1a0874d1db012d2e7fc4f5fa6ec52d2fd7b7
Rod <rod@badlydrawngames.com>
2020-12-17 18:44:19 +0000

    Port controller selection menu to C.

---
+++ spaceships_c/menu.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void InitMenu(void);
+void FiniMenu(void);
+void DrawMenu(double alpha);
+void UpdateMenu(void);
+void HandleEdgeTriggeredEventsMenu(void);
---
+++ spaceships_c/spaceships.c
@@ -0,0 +1,139 @@
+#include "constants.h"
+#include "menu.h"
+#include "raygui.h"
+#include "raylib.h"
+
+#if defined(PLATFORM_WEB) || defined(EMSCRIPTEN)
+#include <emscripten/emscripten.h>
+#endif
+
+#include <math.h>
+
+#define SLOW_FPS 60
+#define FAST_FPS 240
+
+#define MAX_DELTA 0.1f
+
+int renderFps = FAST_FPS;
+
+#if defined(EMSCRIPTEN)
+#define CAP_FRAME_RATE false
+#else
+#define CAP_FRAME_RATE true
+#endif
+
+struct
+{
+    double t;              // Physics time.
+    double updateInterval; // Desired fixed update interval (seconds).
+    double renderInterval; // Desired renderer interval (seconds).
+    double lastTime;       // When did we last try a fixed update?
+    double accumulator;    // How much time was left over?
+    double alpha;          // How far into the next fixed update are we?
+    double lastDrawTime;   // When did we last draw?
+} timing;
+
+void InitTiming(void)
+{
+    timing.t = 0.0;
+    timing.updateInterval = 1.0 / UPDATE_FPS;
+    timing.renderInterval = CAP_FRAME_RATE ? 1.0 / renderFps : 0.0;
+    timing.accumulator = 0.0;
+    timing.alpha = 0.0;
+    timing.lastTime = GetTime();
+    timing.lastDrawTime = timing.lastTime;
+}
+
+void FixedUpdate(void)
+{
+    UpdateMenu();
+}
+
+void Update(double elapsed)
+{
+}
+
+void HandleEdgeTriggeredEvents(void)
+{
+    if (IsKeyPressed(KEY_F11))
+    {
+        ToggleFullscreen();
+    }
+
+    if (IsKeyPressed(KEY_F10))
+    {
+        renderFps = (renderFps == FAST_FPS) ? SLOW_FPS : FAST_FPS;
+        SetTargetFPS(renderFps);
+    }
+
+    HandleEdgeTriggeredEventsMenu();
+}
+
+void Draw(double alpha)
+{
+    DrawMenu(alpha);
+}
+
+void UpdateDrawFrame(void)
+{
+    // See https://gafferongames.com/post/fix_your_timestep/ for more details on how this works.
+    double now = GetTime();
+    double delta = fmin(now - timing.lastTime, MAX_DELTA);
+
+    // Fixed timestep updates.
+    timing.lastTime = now;
+    timing.accumulator += delta;
+    while (timing.accumulator >= timing.updateInterval)
+    {
+        FixedUpdate();
+        timing.t += timing.updateInterval;
+        timing.accumulator -= timing.updateInterval;
+#if defined(EMSCRIPTEN)
+        HandleEdgeTriggeredEvents();
+#endif
+    }
+    timing.alpha = timing.accumulator / timing.updateInterval;
+
+    // Draw, potentially capping the frame rate.
+    now = GetTime();
+    double drawInterval = now - timing.lastDrawTime;
+    if (drawInterval >= timing.renderInterval)
+    {
+        // Per-frame update.
+        Update(fmin(drawInterval, MAX_DELTA));
+
+        // Draw the frame.
+        Draw(timing.alpha);
+        timing.lastDrawTime = now;
+#if !defined(EMSCRIPTEN)
+        HandleEdgeTriggeredEvents(); // As raylib updates input events in EndDrawing(), we update here so as to not miss
+                                     // anything edge triggered such as a keypress when we have a high frame rate.
+#endif
+    }
+}
+
+int main(void)
+{
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Spaceships");
+    SetTargetFPS(renderFps);
+
+    GuiLoadStyle("assets/terminal/terminal.rgs");
+    InitTiming();
+
+    InitMenu();
+
+#if defined(PLATFORM_WEB) || defined(EMSCRIPTEN)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    while (!WindowShouldClose())
+    {
+        UpdateDrawFrame();
+    }
+#endif
+    FiniMenu();
+
+    CloseWindow();
+
+    return 0;
+}
