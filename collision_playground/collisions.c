0aa790b1cfd3b82d42c323be96489634e37080d0
Rod Hyde <rod.hyde@gmail.com>
2021-10-09 00:21:50 +0100

    Fix comment.

--- collision_playground/collisions.c
+++ collision_playground/collisions.c
@@ -102,7 +102,7 @@ bool CheckCollisionMovingAABBs(AABB a, AABB b, Vector2 va, Vector2 vb)
     // An AABB at B's position with the combined size of A and B.
     const AABB aabb = {.centre = b.centre, .extents = Vector2Add(a.extents, b.extents)};
 
-    // A ray at A's position with its direction set to B's velocity relative to A. It's a parametric representation of a
+    // A ray at A's position with its direction set to A's velocity relative to B. It's a parametric representation of a
     // line representing A's position at time t, where 0 <= t <= 1.
     const Ray2D r = {.origin = a.centre, .direction = Vector2Subtract(va, vb)};
 
